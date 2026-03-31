01abed2d0e06e02e46cf9d72cec58f47f72e2fa2
Prajyot Gorlewar <prajyotgorlewar@gmail.com>
2026-04-01 00:18:29 +0530

    added hotellist page

--- src/main/java/com/example/Hotel_Management_Frontend/controller/HotelController.java
+++ src/main/java/com/example/Hotel_Management_Frontend/controller/HotelController.java
@@ -1,10 +1,17 @@
 package com.example.Hotel_Management_Frontend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HotelController {
     @GetMapping("/hotels")
     public String hotels() {
-        return "hotel/hotels";
+        return "hotel/hotelList";
+    }
+
+    @GetMapping("/hotels/details")
+    public String hotelDetails() {
+        return "hotel/hoteldetails";
     }
 }
