47411ca7b4a4a55b8dc926578ae3ec0faa6a8d3c
tanwee14 <8409tan@gmail.com>
2026-03-31 14:44:35 +0530

    added files

--- src/main/java/com/example/Hotel_Management_Frontend/controller/HotelController.java
+++ src/main/java/com/example/Hotel_Management_Frontend/controller/HotelController.java
@@ -1,5 +1,10 @@
 package com.example.Hotel_Management_Frontend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HotelController {
-    
+    @GetMapping("/hotels")
+    public String hotels() {
+        return "hotel/hotels";
+    }
 }
---
+++ src/main/java/com/example/Hotel_Management_Frontend/dto/Reservation.java
@@ -0,0 +1,9 @@
+package com.example.Hotel_Management_Frontend.dto;
+
+
+public class Reservation {
+    private Long id;
+    private String guestName;
+    private String checkIn;
+    private String checkOut;
+}
