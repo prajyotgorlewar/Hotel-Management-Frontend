b1de6b347885b079e6871efeac5bbec266dddc94
Prajyot Gorlewar <prajyotgorlewar@gmail.com>
2026-04-02 14:16:45 +0530

    update url

--- src/main/java/com/example/Hotel_Management_Frontend/service/PaymentService.java
+++ src/main/java/com/example/Hotel_Management_Frontend/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.example.Hotel_Management_Frontend.service;
 
 import com.example.Hotel_Management_Frontend.dto.PaymentDTO.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,20 +14,21 @@ import java.util.List;
 public class PaymentService {
 
     private final RestTemplate restTemplate;
-    private static final String BASE_URL = "http://localhost:8081";
+    private final String baseUrl;
 
-    public PaymentService(RestTemplate restTemplate) {
+    public PaymentService(RestTemplate restTemplate, @Value("${backend.base-url}") String baseUrl) {
         this.restTemplate = restTemplate;
+        this.baseUrl = baseUrl;
     }
 
     public List<PaymentDTO> getPaymentsByHotel(Long hotelId) {
-        String url = BASE_URL + "/payments/search/by-hotel?hotelId=" + hotelId;
+        String url = baseUrl + "/payments/search/by-hotel?hotelId=" + hotelId;
         PaymentWrapper wrapper = restTemplate.getForObject(url, PaymentWrapper.class);
         return wrapper != null ? wrapper.getPayments() : List.of();
     }
 
     public void createPayment(CreatePaymentDTO dto) {
-        String url = BASE_URL +"/payments";
+        String url = baseUrl +"/payments";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<CreatePaymentDTO> request = new HttpEntity<>(dto, headers);
@@ -34,12 +36,12 @@ public class PaymentService {
     }
 
     public PaymentDetailsDTO getPaymentById(Long paymentId) {
-        String url = BASE_URL +"/payments/search/by-payment-id?paymentId=" + paymentId;
+        String url = baseUrl +"/payments/search/by-payment-id?paymentId=" + paymentId;
         return restTemplate.getForObject(url, PaymentDetailsDTO.class);
     }
 
     public void updatePayment(Long paymentId, UpdatePaymentDTO dto) {
-        String url = "http://localhost:8081/payments/" + paymentId;
+        String url = baseUrl + "/payments/" + paymentId;
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<UpdatePaymentDTO> request = new HttpEntity<>(dto, headers);
